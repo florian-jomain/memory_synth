6a630f4bf46c0d4d43fdaf13c8f6bbf2399ab9f9
Florian Jomain <florian.jomain@gmail.com>
2020-04-06 17:39:32 +0200

    Working sounds + events connections

--- js/events.js
+++ js/events.js
@@ -1,4 +1,4 @@
-import * from './sound.js';
+import {c3, cSharp3, d3, dSharp3, e3, f3, fSharp3, g3, gSharp3, a4, aSharp4, b4, c4, cSharp4, d4, dSharp4} from './sound.js';
 
 //Activating main button on HP
 
@@ -19,43 +19,37 @@ import * from './sound.js';
 //---------------------------------------------
 //Keyboard events
 
-let buttons = document.querySelectorAll('.button');
+let button1 = document.querySelector('#btn-1');
+let button2 = document.querySelector('#btn-2');
+let button3 = document.querySelector('#btn-3');
+let button4 = document.querySelector('#btn-4');
+let button5 = document.querySelector('#btn-5');
+let button6 = document.querySelector('#btn-6');
+let button7 = document.querySelector('#btn-7');
+let button8 = document.querySelector('#btn-8');
+let button9 = document.querySelector('#btn-9');
+let button10 = document.querySelector('#btn-10');
+let button11 = document.querySelector('#btn-11');
+let button12 = document.querySelector('#btn-12');
+let button13 = document.querySelector('#btn-13');
+let button14 = document.querySelector('#btn-14');
+let button15 = document.querySelector('#btn-15');
+let button16 = document.querySelector('#btn-16');
 
-// for (let i = 1; i <= 9; i++) {
-//     let `btn${i}` = document.querySelector(`.btn-${i}`);
-// }
-
-let button1 = document.querySelector('.btn-1');
-let button2 = document.querySelector('.btn-2');
-let button3 = document.querySelector('.btn-3');
-let button4 = document.querySelector('.btn-4');
-let button5 = document.querySelector('.btn-5');
-let button6 = document.querySelector('.btn-6');
-let button7 = document.querySelector('.btn-7');
-let button8 = document.querySelector('.btn-8');
-let button9 = document.querySelector('.btn-9');
-let button10 = document.querySelector('.btn-10');
-let button11 = document.querySelector('.btn-11');
-let button12 = document.querySelector('.btn-12');
-let button13 = document.querySelector('.btn-13');
-let button14 = document.querySelector('.btn-14');
-let button15 = document.querySelector('.btn-15');
-let button16 = document.querySelector('.btn-16');
-
-button1.addEventListener('hover', c3);
-button2.addEventListener('hover', cSharp3);
-button3.addEventListener('hover', d3);
-button4.addEventListener('hover', dSharp3);
-button5.addEventListener('hover', e3);
-button6.addEventListener('hover', f3);
-button7.addEventListener('hover', fSharp3);
-button8.addEventListener('hover', g3);
-button9.addEventListener('hover', gSharp3);
-button10.addEventListener('hover', a4);
-button11.addEventListener('hover', aSharp4);
-button12.addEventListener('hover', b4);
-button13.addEventListener('hover', c4);
-button14.addEventListener('hover', cSharp4);
-button15.addEventListener('hover', d4);
-button16.addEventListener('hover', dSharp4);
+button1.addEventListener('click', c3);
+button2.addEventListener('click', cSharp3);
+button3.addEventListener('click', d3);
+button4.addEventListener('click', dSharp3);
+button5.addEventListener('click', e3);
+button6.addEventListener('click', f3);
+button7.addEventListener('click', fSharp3);
+button8.addEventListener('click', g3);
+button9.addEventListener('click', gSharp3);
+button10.addEventListener('click', a4);
+button11.addEventListener('click', aSharp4);
+button12.addEventListener('click', b4);
+button13.addEventListener('click', c4);
+button14.addEventListener('click', cSharp4);
+button15.addEventListener('click', d4);
+button16.addEventListener('click', dSharp4);
 
--- js/sound.js
+++ js/sound.js
@@ -10,15 +10,14 @@ window.addEventListener('click', () => {
 //----------------------------------------------------------
 //Generic variables
 
-let gain = new Tone.Gain(0.3);
+const gain = new Tone.Gain(0.5);
 gain.toMaster();
 
 //----------------------------------------------------------
-//Background sound functions
+// Background sound functions
 
 // let noise = new Tone.Noise('white');
 // let oscillator = new Tone.Oscillator(65, 'sine');
-// let gain = new Tone.Gain(0.3);
 
 // let lowPassFilter = new Tone.LowpassCombFilter(0.1, 1, 100);
 
@@ -42,14 +41,14 @@ gain.toMaster();
 
 const synth = new Tone.Synth({
     'oscillator': {
-        'type': 'pwm',
-        'modulationFrequency': 0.1
+        'type': 'sine',
+        'modulationFrequency': 0.8
     },
     'envelope': {
-        'attack': 0.02,
+        'attack': 0.2,
         'decay': 0.5,
         'sustain': 0.2,
-        'release': 2,
+        'release': 0.5,
     }
 });
 
@@ -58,68 +57,66 @@ synth.connect(gain);
 //----------------------------------------------------------
 //Singular tone functions
 
-function c3() {
+export function c3() {
     synth.triggerAttackRelease('C3', '8n');
 }
 
-function cSharp3() {
+export function cSharp3() {
     synth.triggerAttackRelease('C#3', '8n');
 }
 
-function d3() {
+export function d3() {
     synth.triggerAttackRelease('D3', '8n');
 }
 
-function dSharp3() {
+export function dSharp3() {
     synth.triggerAttackRelease('D#3', '8n');
 }
 
-function e3() {
+export function e3() {
     synth.triggerAttackRelease('E3', '8n');
 }
 
-function f3() {
+export function f3() {
     synth.triggerAttackRelease('F3', '8n');
 }
 
-function fSharp3() {
+export function fSharp3() {
     synth.triggerAttackRelease('F#3', '8n');
 }
 
-function g3() {
+export function g3() {
     synth.triggerAttackRelease('G3', '8n');
 }
 
-function gSharp3() {
+export function gSharp3() {
     synth.triggerAttackRelease('G#3', '8n');
 }
 
-function a4() {
+export function a4() {
     synth.triggerAttackRelease('A4', '8n');
 }
 
-function aSharp4() {
+export function aSharp4() {
     synth.triggerAttackRelease('A#4', '8n');
 }
 
-function b4() {
+export function b4() {
     synth.triggerAttackRelease('B4', '8n');
 }
 
-function c4() {
+export function c4() {
     synth.triggerAttackRelease('C4', '8n');
 }
 
-function cSharp4() {
+export function cSharp4() {
     synth.triggerAttackRelease('C#4', '8n');
 }
 
-function d4() {
+export function d4() {
     synth.triggerAttackRelease('D4', '8n');
 }
 
-function dSharp4() {
+export function dSharp4() {
     synth.triggerAttackRelease('D#4', '8n');
 }
-
-export * from './sound.js';
